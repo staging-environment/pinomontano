9bb8535e683fe7b8b0675784d73a196c286ff620
Bonilla <jarodriguezbonilla@gmail.com>
2026-05-31 02:08:31 +0200

    Force HTTPS when config app.url is HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (str_starts_with(config('app.url'), 'https://')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+            \Illuminate\Support\Facades\URL::forceRootUrl(config('app.url'));
+            $_SERVER['HTTPS'] = 'on';
+            request()->server->set('HTTPS', 'on');
+            request()->headers->set('X-Forwarded-Proto', 'https');
+        }
     }
 }
