6086a5268c7a512fd4c9b49f6d5c25b888c6ed32
Bonilla <jarodriguezbonilla@gmail.com>
2026-07-18 13:55:19 +0200

    Agregar etiqueta link de favicon en todas las vistas

--- resources/views/de-donde-vinimos.blade.php
+++ resources/views/de-donde-vinimos.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="/images/logo_barrio.png">
     <title>De Dónde Vinimos - Raíces Milenarias de Pino Montano</title>
     
     <!-- Fonts -->
--- resources/views/hacia-donde-vamos.blade.php
+++ resources/views/hacia-donde-vamos.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="/images/logo_barrio.png">
     <title>Hacia Dónde Vamos - El Futuro Conectado de Pino Montano</title>
     
     <!-- Fonts -->
